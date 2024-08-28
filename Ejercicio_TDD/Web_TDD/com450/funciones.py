b76db36e3f0c2c741485c3bc1d078c44c642fb4c
Roger Ramos <you@example.com>
2024-08-28 00:08:36 -0400

    Subida de pagina web

--- Ejercicio_TDD/Web_TDD/com450/funciones.py
+++ Ejercicio_TDD/Web_TDD/com450/funciones.py
@@ -1,38 +1,47 @@
-def calcular_A(diplomados=0, especialidades=0, maestrias=0, doctorados=0):
-    puntaje=0
+import numpy as np
+
+def calcular_A(diplomados, especialidades, maestrias, doctorados):
+    #puntajeTotal=0
+    puntajed=0
+    puntajee=0
+    puntajem=0
+    puntajedo=0
     
     # Diplomados
     if diplomados > 0:
-        puntaje += 2
+        puntajed += 2
         if diplomados > 1:
-            puntaje += (diplomados - 1)
-        puntaje = min(puntaje, 4)
+            puntajed += (diplomados - 1)
+        puntajed = min(puntajed, 4)
+        #puntajeTotal+=puntajed
 
     # Especialidades
     if especialidades > 0:
-        puntaje += 4
+        puntajee += 4
         if especialidades > 1:
-            puntaje += (especialidades - 1)*2
-        puntaje = min(puntaje, 6)
+            puntajee += (especialidades - 1)*2
+        puntajee = min(puntajee, 6)
+        #puntajeTotal+=puntajee
 
     # Maestrias
     if maestrias > 0:
-        puntaje += 8
+        puntajem += 8
         if maestrias > 1:
-            puntaje += (maestrias - 1) * 3
-        puntaje = min(puntaje, 11)
+            puntajem += (puntajem - 1) * 3
+        puntajem = min(puntajem, 11)
+        #puntajeTotal+=puntajem
 
     # Doctorados
     if doctorados > 0:
-        puntaje += 12
-        puntaje = min(puntaje, 12)
+        puntajedo += 12
+        puntajedo = min(puntajedo, 12)
+        #puntajeTotal+=puntajedo
 
-    print(puntaje)
-    return puntaje
+    return puntajed,puntajee,puntajem,puntajedo
 
 def calcular_B(asistencia):
     # Asistencias a programas, cursos, seminarios, talleres y congresos.
-    puntaje=0;
+    puntaje=0
 
     if asistencia==1:
         puntaje=0.5
@@ -44,25 +53,31 @@ def calcular_B(asistencia):
     return puntaje
 
 def calcular_C(antiguedad, experiencia, docente):
-    puntaje=0
+    #puntajeTotal=0
+    puntajea=0
+    puntajee=0
+    puntajed=0
 
     if antiguedad > 0:
-        puntaje+=1
+        puntajea+=1
         if antiguedad > 1:
-            puntaje += (antiguedad - 1)
-        puntaje = min(puntaje, 4)
+            puntajea += (antiguedad - 1)
+        puntajea = min(puntajea, 4)
+        #puntajeTotal+=puntajea
 
     if experiencia > 0:
-        puntaje+=1
+        puntajee+=1
         if experiencia > 1:
-            puntaje += (experiencia - 1)
-        puntaje = min(puntaje, 4)
+            puntajee += (experiencia - 1)
+        puntajee = min(puntajee, 4)
+        #puntajeTotal+=puntajee
 
     if docente>=1:
-        puntaje+=2
-        puntaje = min(puntaje, 2)
+        puntajed+=2
+        puntajed = min(puntajed, 2)
+        #puntajeTotal+=puntajed
 
-    return puntaje
+    return puntajea,puntajee,puntajed
 
 def calcular_D(publicacion):
     puntaje=0
@@ -74,6 +89,11 @@ def calcular_D(publicacion):
         puntaje = min(puntaje, 2)
 
     return puntaje
-
-def funcion_calcular_A(diplomados=0, especialidades=0, maestrias=0, doctorados=0):
-    return [0,0,0,12]
+###########################################################################################
+def prueba_calcular_A(diplomados, especialidades, maestrias, doctorados):
+    resultados = np.array([diplomados, especialidades, maestrias, doctorados])
+    print(resultados)
+    if np.array_equal(resultados, [0, 0, 0, 12]):
+        return print("El test de prueba fue correcto.")
+    else:
+        return print("El test de prueba fue incorrecto")
--- Ejercicio_TDD/Web_TDD/com450/views.py
+++ Ejercicio_TDD/Web_TDD/com450/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from .models import Calificacion
-from django.views.decorators.csrf import csrf_exempt
 from . import funciones
 from datetime import datetime
 
@@ -23,18 +22,65 @@ def crear_calificacion(request):
                                     )
         return redirect('index')
 
-@csrf_exempt
+# @csrf_exempt
+# def calculate_puntaje(request):
+#     if request.method == 'POST':
+#         print(request.POST.dict())
+#         input_name = list(request.POST.keys())[0]
+#         value = int(request.POST[input_name])
+#         print(request.POST)
+#         print(request.POST.keys())
+#         print(input_name)
+#         print(value)
+#         puntaje=puntuar(input_name,value)
+#         print(puntaje)
+#         return JsonResponse({'puntaje': puntaje})
+#     return JsonResponse({'error': 'Invalid request'})
+
 def calculate_puntaje(request):
     if request.method == 'POST':
-        input_name = list(request.POST.keys())[0]
-        value = int(request.POST[input_name])
-        print(request.POST.keys())
-        print(input_name)
-        print(value)
-        puntaje=puntuar(input_name,value)
-        print(puntaje)
-        return JsonResponse({'puntaje': puntaje})
-    return JsonResponse({'error': 'Invalid request'})
+        # Extraer los datos del formulario
+        #print(request.POST)
+        
+        diplomado = int(request.POST.get('diplomado', 0))
+        especialidad = int(request.POST.get('especialidad', 0))
+        maestria = int(request.POST.get('maestria', 0))
+        doctorado = int(request.POST.get('doctorado', 0))
+        di,es,ma,do=funciones.calcular_A(diplomado,especialidad,maestria,doctorado)
+        #print(di,es,ma,do)
+        
+        funciones.prueba_calcular_A(di,es,ma,do)
+
+        asistencia = int(request.POST.get('asistencia', 0))
+        asistenciaPuntaje=funciones.calcular_B(asistencia)
+        #print(asistenciaPuntaje)
+
+        antiguedad = int(request.POST.get('antiguedad', 0))
+        servidorPublico = int(request.POST.get('servidorPublico', 0))
+        docente = int(request.POST.get('docente', 0))
+        ant,ser,doc=funciones.calcular_C(antiguedad,servidorPublico,docente)
+        #print(ant,ser,doc)
+
+        articulos = int(request.POST.get('articulos', 0))
+        articulosPuntaje=funciones.calcular_D(articulos)
+        #print(articulosPuntaje)
+
+        form_data = {
+            'diplomadoPuntaje': di,
+            'especialidadPuntaje': es,
+            'maestriaPuntaje': ma,
+            'doctoradoPuntaje': do,
+            'asistenciaPuntaje': asistenciaPuntaje,
+            'antiguedadPuntaje': ant,
+            'servidorPublicoPuntaje': ser,
+            'docentePuntaje': doc,
+            'articulosPuntaje': articulos,
+        }
+        #print(form_data)
+        print("============================================================================================")
+    return JsonResponse(form_data)
+
+
 def puntuar(input_name,valor):
     if(input_name=="diplomado"):
         return funciones.calcular_A(valor,0,0,0)
@@ -54,12 +100,12 @@ def puntuar(input_name,valor):
         return funciones.calcular_C(0,0,valor)
     elif(input_name=="articulos"):
         return funciones.calcular_D(valor)
-    
-    arrayPuntajes=[0,0,0,0]
-    def calcular_prueba(input_name,value):
-        if(input_name=="diplomado" and value==3):
-            arrayPuntajes[1] += value
-            return 0
-        if(input_name=="doctorado" and value==12):
-            arrayPuntajes[4] += value
-        return [0,0,0,12]
+
+arrayPuntajes=[0,0,0,0]
+def calcular_prueba(input_name,value):
+    if(input_name=="diplomado" and value==3):
+        arrayPuntajes[1] += value
+        return 0
+    if(input_name=="doctorado" and value==12):
+        arrayPuntajes[4] += value
+    return [0,0,0,12]
